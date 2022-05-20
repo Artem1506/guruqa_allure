2a1e41eb6e9218db29426295b4bd754ba756729f
Artem_Vaulin <vaulin1506@gmail.com>
2022-05-20 13:59:51 +0300

    add should

--- src/test/java/com/vaulin1506/SelenideTest.java
+++ src/test/java/com/vaulin1506/SelenideTest.java
@@ -1,12 +1,14 @@
 package com.vaulin1506;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideTest {
     String repository = "eroshenkoam/allure-example";
@@ -20,6 +22,6 @@ public class SelenideTest {
         $(".jump-to-field").pressEnter();
         $(byLinkText(repository)).click();
         $(byPartialLinkText("Issue")).click();
-        $(withText("#76")).click();
+        $$(".js-active-navigation-container").find(Condition.text("#76")).shouldBe(visible);
     }
 }
